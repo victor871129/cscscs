8c437b549d8a09bbcc769685201163339d120f69
Hugo <victorhugolafuente@gmail.com>
2021-12-14 09:10:11 -0500

    .

--- cscscs-front/src/Main.js
+++ cscscs-front/src/Main.js
@@ -1,89 +1,114 @@
 import Form from "@rjsf/core";
 import { useEffect } from "react";
 import { parse, stringify } from "css";
-import _ from 'lodash'
+import _ from "lodash";
 
 function Main() {
-    const schema = {
-        required: ["title"],
-        properties: {
-            title: { type: "string", title: "Title", default: "A new task" },
-            done: { type: "boolean", title: "Done?", default: false }
+  const schema = {
+    required: ["title"],
+    properties: {
+      title: { type: "string", title: "Title", default: "A new task" },
+      done: { type: "boolean", title: "Done?", default: false },
+    },
+  };
+
+  const titleItem = (mainPrincipal, firstItem) => {
+    return mainPrincipal.filter(
+      (titleItem) => titleItem !== firstItem && titleItem.length > 0
+    )[0];
+  };
+
+  const loadData = () => {
+    fetch("/cscscs.css")
+      .then((fetchResponse) => {
+        if (!fetchResponse.ok) {
+          throw new Error("Not 2xx response");
         }
-    };
 
-    const titleItem = (mainPrincipal, firstItem) => {
-        return mainPrincipal.filter(titleItem => titleItem !== firstItem && titleItem.length > 0)[0];
-    }
+        return fetchResponse.text();
+      })
+      .then((cssText) => {
+        // TODO check invalid css
+        const parsedObject = parse(cssText);
+        if (
+          parsedObject == null ||
+          parsedObject.type !== "stylesheet" ||
+          (parsedObject.parsingErrors != null &&
+            parsedObject.parsingErrors.length > 0)
+        ) {
+          throw new Error("Stylesheet has errors");
+        }
 
-    const loadData = () => {
-        fetch('/cscscs.css')
-            .then((fetchResponse) => {
-                if (!fetchResponse.ok) {
-                    throw new Error("Not 2xx response")
-                }
+        const sheetRules = parsedObject.stylesheet.rules.filter(
+          (mapItem) => mapItem.type === "rule"
+        );
 
-                return fetchResponse.text();
-            })
-            .then(cssText => {
-                // TODO check invalid css
-                const parsedObject = parse(cssText);
-                if (parsedObject == null || parsedObject.type !== 'stylesheet'
-                    || parsedObject.parsingErrors != null && parsedObject.parsingErrors.length > 0) {
-                    throw new Error("Stylesheet has errors")
-                }
+        if (
+          sheetRules.filter((actualItem) => actualItem.selectors.length > 1)
+            .length > 0
+        ) {
+          throw new Error(
+            "Stylesheet rule hasn't single element in the `selectors` array"
+          );
+        }
 
-                const sheetRules = parsedObject.stylesheet.rules
-                    .filter((mapItem) => mapItem.type === "rule")
+        const dd = sheetRules.map((actualItem) => {
+          const finalValue = {};
+          const primarySelector = actualItem.selectors[0];
+          const mainPrincipal = primarySelector.split(".");
 
-                if (sheetRules.filter((actualItem) => actualItem.selectors.length > 1).length > 0) {
-                    throw new Error("Stylesheet rule hasn't single element in the `selectors` array")
-                }
+          if (mainPrincipal.length < 3) {
+            throw new Error("Invalid principal length");
+          }
 
-                const dd = sheetRules.map((actualItem) => {
-                    const finalValue = {}
-                    const primarySelector = actualItem.selectors[0];
-                    const mainPrincipal = primarySelector.split('.');
+          if (
+            mainPrincipal.filter((titleItem) => titleItem === "string").length >
+            0
+          ) {
+            const itemType = "string";
+            finalValue.type = itemType;
+            finalValue.title = titleItem(mainPrincipal, itemType);
+          } else if (
+            mainPrincipal.filter((titleItem) => titleItem === "data-url")
+              .length > 0
+          ) {
+            const itemType = "data-url";
+            finalValue.type = "string";
+            finalValue.format = itemType;
+            finalValue.title = titleItem(mainPrincipal, itemType);
+          } else if (
+            mainPrincipal.filter((titleItem) => titleItem === "boolean")
+              .length > 0
+          ) {
+            const itemType = "boolean";
+            finalValue.type = itemType;
+            finalValue.title = titleItem(mainPrincipal, itemType);
+          } else {
+            throw new Error("Invalid principal type");
+          }
+          return finalValue;
+        });
 
-                    if (mainPrincipal.length < 3) {
-                        throw new Error("Invalid principal length")
-                    }
+        console.log(
+          "sdfsdfsdf",
+          dd,
+          parsedObject,
+          stringify(parsedObject, { compress: true })
+        );
+      })
+      .catch(console.error);
+  };
 
-                    if (mainPrincipal.filter(titleItem => titleItem === 'string').length > 0) {
-                        const itemType = "string";
-                        finalValue.type = itemType;
-                        finalValue.title = titleItem(mainPrincipal, itemType);
-                    }
-                    else if(mainPrincipal.filter(titleItem => titleItem === 'data-url').length > 0) {
-                        const itemType = "data-url";
-                        finalValue.type = "string";
-                        finalValue.format = itemType;
-                        finalValue.title = titleItem(mainPrincipal, itemType);
-                    }
-                    else if (mainPrincipal.filter(titleItem => titleItem === 'boolean').length > 0) {
-                        const itemType = "boolean";
-                        finalValue.type = itemType;
-                        finalValue.title = titleItem(mainPrincipal, itemType);
-                    }
-                    else {
-                        throw new Error("Invalid principal type")
-                    }
-                    return finalValue;
-                })
+  useEffect(loadData, []);
 
-                console.log("sdfsdfsdf", dd, parsedObject, stringify(parsedObject, { compress: true }));
-            })
-            .catch(console.error);
-    }
-
-    useEffect(loadData, []);
-
-    return (
-        <Form schema={schema}
-            onChange={(x) => console.log(x)}
-            onSubmit={(x) => console.log(x)}
-            onError={(x) => console.log(x)} />
-    );
+  return (
+    <Form
+      schema={schema}
+      onChange={(x) => console.log(x)}
+      onSubmit={(x) => console.log(x)}
+      onError={(x) => console.log(x)}
+    />
+  );
 }
 
 export default Main;
